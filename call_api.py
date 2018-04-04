0533b4a4d563c29bbfcd8e6f37688c3ef11774d5
Kyle Moffett <kyle.moffett@hotmail.com>
2018-04-03 20:55:42 -0700

    testing

--- call_api.py
+++ call_api.py
@@ -7,7 +7,9 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 defaultConfig = config['DEFAULT']
 api_key = str(defaultConfig['api_key'])
-url = "https://api.playbattlegrounds.com/shards/pc-na/matches/276f5bcb-a831-4e8c-a610-d2073692069e"
+#url = "https://api.playbattlegrounds.com/shards/pc-na/matches/276f5bcb-a831-4e8c-a610-d2073692069e"
+#url = "https://api.playbattlegrounds.com/shards/pc-na/players?filter[playerNames]=Dennis_Quaid_419"
+url = "https://api.playbattlegrounds.com/shards/pc-na/matches/0ff52157-03ff-4e03-9bb9-2bed0f8176bc"
 
 header = {
         "Authorization": api_key,
@@ -15,4 +17,13 @@ header = {
 }
 
 r = requests.get(url, headers=header)
-print (r)
+json_response = json.loads(r.text)
+participants = []
+for i in json_response['included']:
+    if i['type'] == "participant":
+        if i['attributes']['stats']['name'] == "Dennis_Quaid_419":
+            participants.append(i)
+for i in json_response['included']:
+    if i['type'] == 'roster':
+        print (json.dumps(i['attributes']))
+print (json.dumps(participants, indent=4))
